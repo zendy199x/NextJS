8e3975076b2e725f31c43304abdab98715476f4a
Zendy <nguyenduc.051194@gmail.com>
2021-09-12 21:59:52 +0700

    Setup project

---
+++ components/NavbarMenu.js
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import { Nav, Navbar } from "react-bootstrap";
+
+const NavbarMenu = () => (
+  <Navbar bg="dark" variant="dark">
+    <Navbar.Brand>My Next App</Navbar.Brand>
+    <Nav>
+      <Link href="/" passHref>
+        <Nav.Link>Home</Nav.Link>
+      </Link>
+      <Link href="/posts" passHref>
+        <Nav.Link>Posts</Nav.Link>
+      </Link>
+      <Link href="/about" passHref>
+        <Nav.Link>About</Nav.Link>
+      </Link>
+    </Nav>
+  </Navbar>
+);
+
+export default NavbarMenu;
---
+++ pages/posts/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../../components/Layout";
+
+const Posts = () => {
+  return (
+    <Layout>
+      <h1>My Posts</h1>
+    </Layout>
+  );
+};
+
+export default Posts;
